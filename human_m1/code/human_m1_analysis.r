7521c5b2fcdd72f85bbb5aaabae82dcd2e7d9694
Nikita Arya <nikitaarya818@gmail.com>
2025-06-11 17:58:48 +0530

    update code

--- human_m1/code/human_m1_analysis.r
+++ human_m1/code/human_m1_analysis.r
@@ -8,9 +8,10 @@ library(pheatmap)
 
 # Read in the metadata file and the gene expression matrix
 meta_filepath <- "/Users/nikit/sequencing data analysis projects in R/metadata.csv"
+matrix_link <- "https://idk-etl-prod-download-bucket.s3.amazonaws.com/aibs_human_m1_10x/matrix.csv"
 
 meta_data <- read.csv(meta_filepath)
-gene_matrix <- read.csv("https://idk-etl-prod-download-bucket.s3.amazonaws.com/aibs_human_m1_10x/matrix.csv")
+gene_matrix <- read.csv(url(matrix_link))
 
 # Exploring the metadata file
 head(meta_data)
@@ -18,4 +19,4 @@ str(meta_data)
 summary(meta_data)
 colnames(meta_data)
 
-meta_data <- meta_data %>% select(!ends_with("_color"))
+meta_data <- meta_data %>% select(!ends_with("_color") & !ends_with("_order"))
