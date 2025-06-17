bbd2289c6f1bf332cf6ab82a3045c8c7c53b7d41
Nikita Arya <nikitaarya818@gmail.com>
2025-06-17 17:58:30 +0530

    Update human_m1_analysis.r

--- human_m1/code/human_m1_analysis.r
+++ human_m1/code/human_m1_analysis.r
@@ -1,8 +1,9 @@
 # Install and load the necessary packages
-#BiocManager::install("DESeq2")
-#install.packages("pheatmap")
+#BiocManager::install(c("DESeq2", "biomaRt"))
+#install.packages(c("pheatmap", "data.table"))
 
 library(DESeq2)
+library(biomaRt)
 library(tidyverse)
 library(pheatmap)
 
@@ -11,7 +12,7 @@ meta_filepath <- "/Users/nikit/sequencing data analysis projects in R/metadata.c
 matrix_link <- "https://idk-etl-prod-download-bucket.s3.amazonaws.com/aibs_human_m1_10x/matrix.csv"
 
 meta_data <- read.csv(meta_filepath)
-gene_matrix <- read.csv(url(matrix_link))
+gene_matrix <- data.table::fread(matrix_link)
 
 # Exploring the metadata file
 head(meta_data)
@@ -20,3 +21,7 @@ summary(meta_data)
 colnames(meta_data)
 
 meta_data <- meta_data %>% select(!ends_with("_color") & !ends_with("_order"))
+
+# Exploring the gene matrix file
+head(gene_matrix)
+str(gene_matrix)
